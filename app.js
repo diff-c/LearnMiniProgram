c1b82c084a6c2619b70bcf410165eba48994dfc7
diff-c <2868231146@qq.com>
2020-01-16 17:49:46 +0800

    组件(scroll,input,image)使用

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ App({
   //生命周期函数
   //小程序初始化
   onLaunch() {
-    console.log('小程序初始化')
+    // console.log('小程序初始化')
     //网络请求
     //获取用户的信息
 
     //wx是系统API
     wx.getUserInfo({
      success(res) {
-       console.log(res)
+      //  console.log(res)
      }
     })
   },
@@ -22,8 +22,8 @@ App({
   onShow(options) {
     //1.判断小程序的进入场景
 
-    console.log(options)
-    console.log('页面显示')
+    // console.log(options)
+    // console.log('页面显示')
   },
   //小程序隐藏时
   onHide() {
---
+++ pages/image/image.js
@@ -0,0 +1,80 @@
+// pages/image/image.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imageUrl: ''
+  },
+  imageLoad() {
+    console.log('图片加载完成')
+  },
+  chooseImage() {
+    //调用系统API，选中图片
+    wx.chooseImage({
+      success: res => {
+        this.setData({
+          imageUrl:res.tempFiles[0].path
+        })
+      }
+    })
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
